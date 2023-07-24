a3d3c5c9ee7c2e12b3f0009ef65fa16d9341c16c
Raúl Pérez <jrpcassone@gmail.com>
2023-07-24 20:34:52 +0200

    Add types and types index

---
+++ src/ts/data/index.ts
@@ -0,0 +1,36 @@
+import { type CharacterData } from "../types";
+
+export const joffrey: CharacterData = {
+  name: "Joffrey",
+  family: "Baratheon",
+  age: 16,
+  imageSource: "joffrey.jpg",
+};
+
+export const jaime = {
+  name: "Jaime",
+  family: "Lannister",
+  age: 45,
+  imageSource: "jaime.jpg",
+};
+
+export const jaenerys = {
+  name: "Daenerys",
+  family: "Targaryen",
+  age: 30,
+  imageSource: "daenerys.jpg",
+};
+
+const tyrion = {
+  name: "Tyrion",
+  family: "Lannister",
+  age: 40,
+  imageSource: "tyrion.jpg",
+};
+
+const bronn = {
+  name: "Bronn",
+  family: "Blackwater",
+  age: 54,
+  imageSource: "bronn.jpg",
+};
