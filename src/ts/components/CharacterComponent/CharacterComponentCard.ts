c72938b876f50d8baf33d4239d5d6dc797a5c127
Raúl Pérez <jrpcassone@gmail.com>
2023-07-25 21:38:46 +0200

    Fix card positions and hover

--- src/ts/components/CharacterComponent/CharacterComponentCard.ts
+++ src/ts/components/CharacterComponent/CharacterComponentCard.ts
@@ -10,32 +10,47 @@ class CharacterComponentCard extends Component {
     private parentElement: Element,
     character: King | Fighter | Squire
   ) {
-    super(parentElement, "article", "character");
+    super(parentElement, "li", "col");
 
     this.character = character;
   }
 
   render() {
     this.element.innerHTML = ` 
-     
-        <div class="card character__card">
-          <img src="${this.character.characterData.imageSource}" alt="Character's Name and family" class="character__picture card-img-top" />
-          <div class="card-body">
-            <h2 class="character__name card-title h4">${this.character.characterData.name} ${this.character.characterData.family} </h2>
-            <div class="character__info">
-              <ul class="list-unstyled">
-                <li>Age: ${this.character.characterData.age}</li>
-                <li>
-                  State:
-                  <i class="fas fa-thumbs-down"></i>
-                  <i class="fas fa-thumbs-up"></i>
-                </li>
-              </ul>
+    <article class="character">
+          <div class="card character__card">
+            <img src="img/${this.character.characterData.imageSource}" alt="Character's Name and family" class="character__picture card-img-top" />
+            <div class="card-body">
+              <h2 class="character__name card-title h4">${this.character.characterData.name}
+        ${this.character.characterData.family}</h2>
+              <div class="character__info">
+                <ul class="list-unstyled">
+                  <li>Age: ${this.character.characterData.age}</li>
+                  <li>
+                    State:
+                    <i class="fas fa-thumbs-down"></i>
+                    <i class="fas fa-thumbs-up"></i>
+                  </li>
+                </ul>
+              </div>
+              <div class="character__overlay">
+                <ul class="list-unstyled">
+                  <li>Years of Reign: X</li>
+                  <li>Weapon: XXX</li>
+                  <li>Dexterity: X</li>
+                  <li>Kiss Ass level: X</li>
+                  <li>Advises to: X</li>
+                  <li>Serves to: X</li>
+                </ul>
+                <div class="character__actions">
+                  <button class="character__action btn">speak</button>
+                  <button class="character__action btn">die</button>
+                </div>
+              </div>
             </div>
+            <i class="emoji"></i>
           </div>
-        </div>
-     
-      </li>`;
+        </article>`;
   }
 }
 
